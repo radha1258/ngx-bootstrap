e91704c4ce19b4a8119f1bd2ed76c454ccb32e8f
mohanarao <nerjinaveen21@gmail.com>
2019-05-12 22:40:19 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { setTheme } from 'ngx-bootstrap/utils';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,55 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngx-bootstrap-app';
+  dropDownMenuClick: boolean;
+  bills: any = [
+    {
+      payee: "Youtube TV",
+      account: "Account Name (****1604)"
+    },
+    {
+      payee: "Sling",
+      account: "Account Name (****1604)"
+    },
+    {
+      payee: "ABC",
+      account: "Account Name (****1604)"
+    },
+    {
+      payee: "ABC",
+      account: "Account Name (****1604)"
+    },
+    {
+      payee: "ABC",
+      account: "Account Name (****1604)"
+    },
+    {
+      payee: "ABC",
+      account: "Account Name (****1604)"
+    },
+    {
+      payee: "ABC",
+      account: "Account Name (****1604)"
+    }
+  ]
+  items: string[] = [
+    'Create One-Time Payment',
+    'Create Recurring Payment',
+    'Edit Payee',
+    'Delete Payee'
+  ];
+
+  constructor(){
+    setTheme('bs4');
+  }
+ 
+  onHidden(): void {
+    console.log('Dropdown is hidden');
+  }
+  onShown(): void {
+    console.log('Dropdown is shown');
+  }
+  isOpenChange(): void {
+    console.log('Dropdown state is changed');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFontAwesomeModule,
+    TabsModule.forRoot(),
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
